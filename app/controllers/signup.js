b410e75202e07f607d8496973914bea815b3b102
getabetterpic <dan@danandjensmith.com>
2016-02-14 20:25:03 -0500

    Add user signup

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -2,26 +2,31 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
   actions: {
     signup() {
+      this.set('hasError', false);
       const self = this;
       const email = this.get('identification');
-      if (Ember.empty(email)) {
-        $('identification').data('error', 'Email can\'t be blank');
-        return;
+      if (Ember.isEmpty(email)) {
+        Materialize.toast("Email can't be blank", 4000);
+        this.set('hasError', true);
       }
       const password = this.get('password');
       const passwordConfirmation = this.get('passwordConfirmation');
       if (password !== passwordConfirmation) {
-        $('password-confirmation').data('error', 'Confirmation must match password');
-        return;
-      } else {
+        Materialize.toast("Passwords do not match", 4000);
+        this.set('hasError', true);
+      }
+      if (!this.get('hasError')) {
         let person = this.store.createRecord('person', {
           email: email,
           password: password,
-          passwordConfirmation: password
+          password_confirmation: password
         });
         person.save().then(function() {
+          self.get('session').authenticate('authenticator:devise', email, password);
+          self.store.unloadAll('person');
           self.transitionToRoute('accounts');
         });
       }
