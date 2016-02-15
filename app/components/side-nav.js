efe29688c24575db315e699705e503882faa1fb1
getabetterpic <dan@danandjensmith.com>
2016-02-14 20:34:05 -0500

    Move session invalidate to nav-links

--- app/components/side-nav.js
+++ app/components/side-nav.js
@@ -2,18 +2,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  session: Ember.inject.service('session'),
 
   didInsertElement() {
     $('.button-collapse').sideNav({
       closeOnClick: true
     });
     $('.dropdown-button').dropdown();
-  },
-
-  actions: {
-    invalidateSession() {
-      this.get('session').invalidate();
-    }
   }
 });
